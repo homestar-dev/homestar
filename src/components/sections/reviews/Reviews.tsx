8b55c69e137eb89f4c25022c4caa3573a922025a
berenyilevente <berenyi.lev@gmail.com>
2023-10-06 11:15:14 +0200

    fix: order of reviews

--- src/components/sections/reviews/Reviews.tsx
+++ src/components/sections/reviews/Reviews.tsx
@@ -5,13 +5,16 @@ export const Reviews: React.FC = () => {
   return (
     <div>
       <div className="bg-grey-100 grid xl:grid-cols-2 justify-items-center items-center xl:pt-12 xl:px-48 px-4 ">
-        <div className="xl:col-span-1 col-span-2 mb-8 sm:m-4">
-          <ReviewStepper />
+        <div className="col-span-2 justify-items-center items-center flex md:flex-nowrap flex-wrap-reverse">
+          <div className="mb-8 sm:m-4">
+            <ReviewStepper />
+          </div>
+          <div className="md:text-2xl text-xl md:w-3/4 sm:mx-20 text-center leading-relaxed tracking-widest font-semibold italic pb-4 font-futura-italic py-8">
+            Kíváncsi vagy, milyen lesz velünk dolgozni? Beszéljenek helyettünk
+            eddigi ügyfeleink véleményei!
+          </div>
         </div>
-        <div className="md:text-2xl text-xl sm:w-3/4 text-center leading-relaxed tracking-widest font-semibold italic pb-4 font-futura-italic xl:col-span-1 col-span-2">
-          Kíváncsi vagy, milyen lesz velünk dolgozni? Beszéljenek helyettünk
-          eddigi ügyfeleink véleményei!
-        </div>
+
         <div className="col-span-2 text-center mt-10 mb-4 md:text-4xl text-2xl font-futura-medium ">
           Hirdess ott, ahol a vendégeid keresnek!
         </div>
