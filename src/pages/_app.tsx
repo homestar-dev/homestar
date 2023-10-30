9230db4b4d466bc1869aa8d45586c266d8159e27
berenyilevente <berenyi.lev@gmail.com>
2023-10-30 11:30:27 +0100

    feat: added hotjar tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,17 @@ import { ConfirmDataUsage, GoogleAnalytics, Navigation } from "@/components";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const hotjarHJID = 3704257;
+  const hotjarHJSV = 6;
+
+  useEffect(() => {
+    hotjar.initialize(hotjarHJID, hotjarHJSV);
+  }, []);
+
   return (
     <>
       <GoogleAnalytics googleAnalyticsId="G-P8PT20C0G5" />
