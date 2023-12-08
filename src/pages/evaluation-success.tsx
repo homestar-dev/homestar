cb29250c272ff3c02292a9e6d5cb65382f420460
berenyilevente <berenyi.lev@gmail.com>
2023-12-08 11:41:41 +0100

    fix: google tag

--- src/pages/evaluation-success.tsx
+++ src/pages/evaluation-success.tsx
@@ -1,6 +1,20 @@
 import { FormSuccess } from "@/components";
+import Script from "next/script";
 import React from "react";
 
 export default function EvaluationSuccess() {
-  return <FormSuccess />;
+  return (
+    <>
+      <Script id="google-conversion" strategy="afterInteractive">
+        {`
+      gtag('event', 'conversion', {
+        'send_to': 'AW-11268532703',
+        'value': 1.0,
+        'currency': 'USD'
+      });
+    `}
+      </Script>
+      <FormSuccess />
+    </>
+  );
 }
